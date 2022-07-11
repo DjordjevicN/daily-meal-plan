791659cf07173618a80b009ffe76869361a5643e
Nikola <nikola.dj.87@gmail.com>
2022-07-11 22:05:59 +0200

    Added modal placeholder to the table of shopp

--- src/components/tableRow/TableRow.tsx
+++ src/components/tableRow/TableRow.tsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TableRow.scss";
 
 const TableRow = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   return (
-    <div className="tableRow">
-      <div className="status"></div>
-      <div className="avatar">
-        <img src="" alt="" />
+    <>
+      <div className="tableRow" onClick={() => setIsOpen(true)}>
+        <div className="status"></div>
+        <div className="avatar">
+          <img src="" alt="" />
+        </div>
+        <div className="name">
+          <p>Avokado</p>
+        </div>
+        <div className="purchaseAmount">
+          <p>1 kom</p>
+        </div>
+        <div className="currentPrice">
+          <p>180 din</p>
+        </div>
+        <div className="currentAmount">
+          <p>0 kom</p>
+        </div>
       </div>
-      <div className="name">
-        <p>Avokado</p>
-      </div>
-      <div className="purchaseAmount">
-        <p>1 kom</p>
-      </div>
-      <div className="currentPrice">
-        <p>180 din</p>
-      </div>
-      <div className="currentAmount">
-        <p>0 kom</p>
-      </div>
-    </div>
+      {isOpen && (
+        <div className="buyModal">
+          <div className="content">
+            <p onClick={() => setIsOpen(false)}>x</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
