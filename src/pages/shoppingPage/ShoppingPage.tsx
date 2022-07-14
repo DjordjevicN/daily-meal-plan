612756f69d736e759a5d19792f950e71ecfbe87a
Nikola <nikola.dj.87@gmail.com>
2022-07-14 15:52:57 +0200

    mob responsive

--- src/pages/shoppingPage/ShoppingPage.tsx
+++ src/pages/shoppingPage/ShoppingPage.tsx
@@ -64,98 +64,121 @@ const ShoppingPage = () => {
               <form onSubmit={handleAddNew}>
                 <div className="inputBlock">
                   <p className="inputLabel">Name</p>
-                  <input
-                    type="text"
-                    value={modalState.name}
-                    onChange={(e) => {
-                      setModalState({ ...modalState, name: e.target.value });
-                    }}
-                  />
-                </div>
-                <div className="inputBlock">
-                  <p className="inputLabel">Price</p>
-                  <input
-                    type="number"
-                    value={modalState.price}
-                    onChange={(e) => {
-                      setModalState({ ...modalState, price: +e.target.value });
-                    }}
-                  />
-                </div>
-                <div className="inputBlock">
-                  <p className="inputLabel">Base amount</p>
-                  <input
-                    type="number"
-                    value={modalState.base_amount}
-                    onChange={(e) => {
-                      setModalState({
-                        ...modalState,
-                        base_amount: +e.target.value,
-                      });
-                    }}
-                  />
-                </div>
-                <div className="inputBlock">
-                  <p className="inputLabel">Current amount</p>
-                  <input
-                    type="number"
-                    value={modalState.current_amount}
-                    onChange={(e) => {
-                      setModalState({
-                        ...modalState,
-                        current_amount: +e.target.value,
-                      });
-                    }}
-                  />
-                </div>
-                <div className="inputBlock">
-                  <p className="inputLabel">Calories</p>
-                  <input
-                    type="number"
-                    value={modalState.calories}
-                    onChange={(e) => {
-                      setModalState({
-                        ...modalState,
-                        calories: +e.target.value,
-                      });
-                    }}
-                  />
-                </div>
-                <div className="inputBlock">
-                  <p className="inputLabel">Carbs</p>
-                  <input
-                    type="number"
-                    value={modalState.carbs}
-                    onChange={(e) => {
-                      setModalState({ ...modalState, carbs: +e.target.value });
-                    }}
-                  />
-                </div>
-                <div className="inputBlock">
-                  <p className="inputLabel">Fat</p>
-                  <input
-                    type="number"
-                    value={modalState.fat}
-                    onChange={(e) => {
-                      setModalState({ ...modalState, fat: +e.target.value });
-                    }}
-                  />
+                  <div className="inputEl">
+                    <input
+                      type="text"
+                      value={modalState.name}
+                      onChange={(e) => {
+                        setModalState({ ...modalState, name: e.target.value });
+                      }}
+                    />
+                  </div>
                 </div>
 
                 <div className="inputBlock">
-                  <p className="inputLabel">Current amount</p>
-                  <input
-                    alt="avatar"
-                    type="file"
-                    onChange={(e) => {
-                      setModalState({
-                        ...modalState,
-                        img: e.target.value,
-                      });
-                    }}
-                  />
+                  <p className="inputLabel">Price</p>
+                  <div className="inputEl">
+                    <input
+                      type="number"
+                      value={modalState.price}
+                      onChange={(e) => {
+                        setModalState({
+                          ...modalState,
+                          price: +e.target.value,
+                        });
+                      }}
+                    />
+                  </div>
                 </div>
                 <div className="inputBlock">
+                  <p className="inputLabel">Base amount</p>
+                  <div className="inputEl">
+                    <input
+                      type="number"
+                      value={modalState.base_amount}
+                      onChange={(e) => {
+                        setModalState({
+                          ...modalState,
+                          base_amount: +e.target.value,
+                        });
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className="inputBlock">
+                  <p className="inputLabel">Current amount</p>
+                  <div className="inputEl">
+                    <input
+                      type="number"
+                      value={modalState.current_amount}
+                      onChange={(e) => {
+                        setModalState({
+                          ...modalState,
+                          current_amount: +e.target.value,
+                        });
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className="inputBlock">
+                  <p className="inputLabel">Calories</p>
+                  <div className="inputEl">
+                    <input
+                      type="number"
+                      value={modalState.calories}
+                      onChange={(e) => {
+                        setModalState({
+                          ...modalState,
+                          calories: +e.target.value,
+                        });
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className="inputBlock">
+                  <p className="inputLabel">Carbs</p>
+                  <div className="inputEl">
+                    <input
+                      type="number"
+                      value={modalState.carbs}
+                      onChange={(e) => {
+                        setModalState({
+                          ...modalState,
+                          carbs: +e.target.value,
+                        });
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className="inputBlock">
+                  <p className="inputLabel">Fat</p>
+                  <div className="inputEl">
+                    <input
+                      type="number"
+                      value={modalState.fat}
+                      onChange={(e) => {
+                        setModalState({ ...modalState, fat: +e.target.value });
+                      }}
+                    />
+                  </div>
+                </div>
+
+                <div className="inputBlock">
+                  <p className="inputLabel">Add image</p>
+                  <div className="inputEl avatar-input">
+                    <input
+                      alt="avatar"
+                      type="file"
+                      onChange={(e) => {
+                        setModalState({
+                          ...modalState,
+                          img: e.target.value,
+                        });
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className="inputBlock button">
                   <input type="submit" value="Add" />
                 </div>
               </form>
