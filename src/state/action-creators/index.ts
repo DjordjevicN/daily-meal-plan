3222bf23d555ebc0df7b57738985deaee6e8a2fb
Nikola <nikola.dj.87@gmail.com>
2022-07-16 01:11:07 +0200

    f

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -4,7 +4,7 @@ import { Dispatch } from "redux";
 import axios from "axios";
 
 // const baseUrl = "http://localhost:3001";
-const baseUrl = "http://jelovnik.nikola-djordjevic.com/get_all_ingredients";
+const baseUrl = "https://jelovnik.nikola-djordjevic.com";
 
 export const withdrawMoney = (amount: number) => {
   return (dispatch: Dispatch<Action>) => {
