461fce6179608ac47e0eb3bc1e275a37aa7af6ed
Nikola <nikola.dj.87@gmail.com>
2022-07-18 09:55:37 +0200

    try hash to fix reload 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-// import React, { useEffect } from "react";
 import "./App.scss";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homePage/HomePage";
 import LoginPage from "./pages/loginPage/LoginPage";
 import ShoppingPage from "./pages/shoppingPage/ShoppingPage";
 import Menu from "./components/menu/Menu";
-// import { useDispatch, useSelector } from "react-redux";
-// import { bindActionCreators } from "redux";
-// import { actionCreators, State } from "./state";
-function App() {
-  // const dispatch = useDispatch();
-  // const { getAllIngredients } = bindActionCreators(actionCreators, dispatch);
-  // const state = useSelector((state: State) => {
-  //   return state;
-  // });
-  // console.log(state);
-  // useEffect(() => {
-  //   getAllIngredients();
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
 
+function App() {
   return (
     <div className="App">
       <Menu />
