9ae4f9fba7e3ac04394ac4ff69733b9ac128d236
Nikola <nikola.dj.87@gmail.com>
2022-08-26 02:55:55 +0200

    Daily meal design done
    -Redesigned cards and page

--- src/UiComponents/atom/ButtonShell/ButtonShell.tsx
+++ src/UiComponents/atom/ButtonShell/ButtonShell.tsx
@@ -5,11 +5,17 @@ interface IProps {
   type?: string;
   icon?: any;
   customStyle?: any;
+  onClick?: () => void;
 }
 
 const ButtonShell = (props: IProps) => {
   return (
     <div
+      onClick={() =>
+        props.onClick
+          ? props.onClick()
+          : console.warn("You forgot to put action on button")
+      }
       id="ButtonShell"
       className={props.type ?? "default"}
       style={props.customStyle}
--- src/components/meal/Meal.tsx
+++ src/components/meal/Meal.tsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 
 import "./Meal.scss";
 import { baseUrl } from "../../constants/utilFunc";
-import FoodItems from "../foodItems/FoodItems";
-import { FiRefreshCcw } from "react-icons/fi";
 import axios from "axios";
 import { mealConst } from "../../constants/mealConst";
+
+import ButtonShell from "../../UiComponents/atom/ButtonShell/ButtonShell";
+import { GoCheck } from "react-icons/go";
 
 interface IMeal {
   amount: number;
@@ -22,6 +23,7 @@ type Props = {
 const initState = {
   img: "",
   name: "",
+  calories: "",
 };
 
 const getMeal = async (id: number) => {
@@ -36,20 +38,54 @@ const getMeal = async (id: number) => {
 
 const Meal: React.FC<Props> = ({ meal }) => {
   const [singleMeal, setSingleMeal] = useState(initState);
+  const [iEatThis, setIEatThis] = useState(false);
   useEffect(() => {
     getMeal(meal.meal_id).then(setSingleMeal);
   }, [meal.meal_id]);
+  console.log(meal);
+  console.log(singleMeal);
 
   return (
     <div className="meal__wrapper ">
       <div className="meal_content">
-        <div className="nameAndAction">
-          <h3 className="meal_name">{mealConst[meal.meal_type]}</h3>
-          <button>I ate this</button>
-          <FiRefreshCcw className="icon" />
+        <div className="image">
+          <img
+            src={
+              singleMeal.img
+                ? `${baseUrl()}/uploads/${singleMeal.img}`
+                : "images/noimage.png"
+            }
+            alt="meal"
+          />
         </div>
-        <p className="calories_count">{`Calories`}</p>
-        {singleMeal && <FoodItems meal={meal} food={singleMeal} />}
+        <div className="split">
+          <div className="Information">
+            <p className="mealType">{mealConst[meal.meal_type]}</p>
+            <p className="mealName">{singleMeal.name}</p>
+            <p className="mealServing">
+              Serving <span>{`${meal.amount} ${meal.unit}`}</span>
+            </p>
+            <p className="mealCalories">
+              Calories <span>{singleMeal.calories ?? "No data"}</span>
+            </p>
+          </div>
+          <div className="action">
+            <ButtonShell
+              onClick={() => setIEatThis(!iEatThis)}
+              type="mono"
+              customStyle={{
+                width: "50px",
+                height: "50px",
+                padding: "10px",
+                borderRadius: "8px",
+                boxShadow: `0px 0px 2px #a8a8a8, 0px 0px 0px 5px ${
+                  iEatThis ? "#c0edc0" : "#fff"
+                },8px 8px 15px #a7aaaf, -8px -8px 15px #fff`,
+              }}
+              icon={<GoCheck />}
+            ></ButtonShell>
+          </div>
+        </div>
       </div>
     </div>
   );
