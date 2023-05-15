b03f8bd81011cef0319c6f06ce29fb384e946aff
Nikola <nikola.dj.87@gmail.com>
2023-05-15 22:09:40 +0200

    setting dev env

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,30 +1,14 @@
 import "./HomePage.scss"
 import Navigation from "../../UiComponents/organism/Navigation/Navigation"
-import Calculator from "../../UiComponents/organism/Calculator/Calculator"
-import ButtonShell from "../../UiComponents/atom/ButtonShell/ButtonShell"
-import { motion, AnimatePresence } from "framer-motion"
-
-import { Link } from "react-router-dom"
 import { useSelector } from "react-redux"
 
-const deviceAnimation = {
-  open: {
-    right: "130px",
-    transform: ["rotate(-20deg)", "rotate(0deg)"],
-    opacity: 1,
-  },
-  closed: {
-    width: "0%",
-  },
-}
-
 function HomePage() {
-  // const counter = useSelector((state: any) => state.counter)
-  // console.log(counter)
+  const counter = useSelector((state: any) => state.test)
 
   return (
     <>
       <Navigation />
+      <h1>{counter.value}</h1>
       <div className="homepage">homepage</div>
     </>
   )
