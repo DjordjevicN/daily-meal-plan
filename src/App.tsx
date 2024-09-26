46cb946608b36301a30df1605c982643dd88fe8f
Johnny3223 <johnny@streamflow.finance>
2024-09-26 17:31:13 +0200

    cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,39 @@
-import "./App.scss"
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/homePage/HomePage"
-import ShoppingPage from "./pages/shoppingPage/ShoppingPage"
-import SingleMealPage from "./pages/singleMealPage/SingleMealPage"
-import UserProfilePage from "./pages/userProfilePage/UserProfilePage"
-import Dashboard from "./pages/dashboard/Dashboard"
-import SignIn from "./pages/signIn/SignIn"
-import { useDispatch } from "react-redux"
-import { actionCreators } from "./state"
-import { bindActionCreators } from "redux"
-import Login from "./pages/Login/Login"
-import FoodTransfer from "./pages/FoodTransfer/FoodTransfer"
+import { Routes } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { actionCreators } from "./state";
+import { bindActionCreators } from "redux";
+import construction from "../src/assets/Construction/construction.webp";
 
 function App() {
-  const dispatch = useDispatch()
-  const { getUser } = bindActionCreators(actionCreators, dispatch)
+  const dispatch = useDispatch();
+  const { getUser } = bindActionCreators(actionCreators, dispatch);
   if (localStorage.getItem("userId")) {
-    getUser(localStorage.getItem("userId"))
+    getUser(localStorage.getItem("userId"));
+  }
+  const featureGate = localStorage.getItem("featureGate");
+  if (!featureGate) {
+    return (
+      <div className="w-[100vh] h-[100vh] flex items-center justify-center">
+        <img className="w-[50%]" src={construction} alt="" />
+      </div>
+    );
   }
 
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/shopping" element={<ShoppingPage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        {/* <Route path="/shopping" element={<ShoppingPage />} />
         <Route path="/profile" element={<UserProfilePage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/login" element={<Login />} />
         <Route path="/meal/:id" element={<SingleMealPage />} />
         <Route path="/foodTransfer" element={<FoodTransfer />} />
-
-        <Route path="*" element={<HomePage />} />
+        <Route path="*" element={<HomePage />} /> */}
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
