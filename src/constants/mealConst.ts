5848c0c6942c5843b33604ac8fb0c09f838d7015
Johnny3223 <johnny@streamflow.finance>
2024-09-26 17:53:34 +0200

    new install

--- src/constants/mealConst.ts
+++
@@ -1,7 +0,0 @@
-export const mealConst: { [key: number]: string } = {
-  0: "Breakfast",
-  1: "Soup",
-  2: "Lunch",
-  3: "Sneak",
-  4: "Diner",
-};
--- src/constants/pages.ts
+++
@@ -1,5 +0,0 @@
-export const pages = {
-  MEAL_PLAN: "meal_plan",
-  SHOPPING: "shopping",
-  CREATOR: "creator",
-};
